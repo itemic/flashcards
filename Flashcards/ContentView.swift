703dcb4d48197209be58432a86e441673b746711
Terran Kroft <terrankroft@gmail.com>
2020-07-16 16:22:28 +0800

    Initial Commit

---
+++ Flashcards/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Flashcards
+//
+//  Created by Terran Kroft on 7/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Terran Kroft on 7/16/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
